0a21bc914421dd889fbfc5b0fdad3f76b0f84624
Jacob Windsor <me@jcbwndsr.com>
2022-03-04 13:15:33 +0100

    dcp-687 Refactor observation into an IngestObservation subclass

--- hca_cellxgene/observation.py
+++ hca_cellxgene/observation.py
@@ -1,5 +1,6 @@
+import logging
 import os
-from typing import Union
+from typing import Union, Optional
 
 import pandas as pd
 import requests
@@ -8,12 +9,10 @@ from ingest.api.ingestapi import IngestApi
 from pandas import DataFrame
 
 
-
 class Observation:
-    def __init__(self, data):
+    def __init__(self, **kwargs):
         load_dotenv()
-        self.data = data
-        fields = [
+        self.fields = [
             'assay_ontology_term_id',
             # Ignoring the below for now: talk to Wei
             # 'cell_type_ontology_term_id',
@@ -26,15 +25,73 @@ class Observation:
             'tissue_ontology_term_id',
         ]
 
-        # for field in fields:
-        #     self[field] = kwargs.get(field, 'UNSET')
+        for field in self.fields:
+            self.__setattr__(field, kwargs.get(field, 'UNSET') or 'UNSET')
 
-    def __setitem__(self, key, value):
-        self[key] = value
+    def to_data_frame(self) -> DataFrame:
+        to_display = {key: self.__dict__[key] for key in self.fields}
+        return pd.DataFrame(to_display, index=[0])
 
+
+class IngestObservation(Observation):
+    def __init__(self, cell_suspension_uuid):
+        self.cell_suspension_uuid = cell_suspension_uuid
+
+        ingest_base = os.environ.get('INGEST_API')
+        self.ingest_api = IngestApi(ingest_base)
+
+        self.__build_biomaterial_chain()
+
+        # There must always be a donor and a specimen but cell line and organoid are optional parents
+        specimen_from_organism = self.__get_entity_with_type('specimen_from_organism')
+        donor_organism = self.__get_entity_with_type('donor_organism')
+        lib_prep = self.__get_entity_with_type('library_preparation_protocol')
+
+        diseases = specimen_from_organism['content']['diseases']
+        if len(diseases) > 1:
+            # ASSUMPTION: Use first disease, assuming all diseases are equal
+            logging.warning(
+                f'Biomaterial {specimen_from_organism["uuid"]["uuid"]} has multiple diseases. Using the first.'
+            )
+
+        super().__init__(**{
+            'assay_ontology_term_id': lib_prep['content']['library_construction_method']['text'],
+            'development_stage_ontology_term_id:human': donor_organism['content']['development_stage']['text'],
+            'disease_ontology_term_id': diseases[0]['text'],
+            'ethnicity_ontology_term_id:human': donor_organism['content']['human_specific']['ethnicity'][0]['text'],
+            'is_primary_data': True,
+            'organism_ontology_term_id': donor_organism['content']['genus_species'][0]['text'],
+            'sex_ontology_term_id': donor_organism['content']['sex'],
+            'tissue_ontology_term_id': self.__get_tissue_ontology_term()
+        })
+
+    def __get_cell_suspension(self):
+        return self.ingest_api.get_entity_by_uuid('biomaterials', self.cell_suspension_uuid)
 
     @staticmethod
-    def __get_entity_from_link(entity: dict, link, allow_multiple=False) -> Union[list[dict], dict]:
+    def __get_lib_prep_for_cell_suspension(cell_suspension) -> dict:
+        input_to = IngestObservation.__get_entities_from_link(cell_suspension, 'inputToProcesses')
+
+        lib_prep = None
+        for process in input_to:
+            output_protocols = IngestObservation.__get_entities_from_link(process, 'protocols')
+            lib_preps = [x for x in output_protocols if 'library_preparation_protocol' in x['content']['describedBy']]
+
+            if len(lib_preps) > 1:
+                raise IndexError(
+                    f"Process {process['uuid']['uuid']} should only have one library preparation protocol."
+                )
+
+            if lib_prep and lib_preps[0]['uuid']['uuid'] != lib_prep['uuid']['uuid']:
+                raise TypeError("Cell suspension should only be associated to one library preparation protocol.")
+
+            lib_prep = lib_preps[0]
+        return lib_prep
+
+    @staticmethod
+    def __get_entities_from_link(entity: dict, link: str) -> Union[list[dict], dict]:
+        logging.info(f'Getting {link} for {IngestObservation.__get_type_of_entity(entity)} {entity["uuid"]["uuid"]}')
+
         r = requests.get(
             entity['_links'][link]['href']
         )
@@ -42,96 +99,84 @@ class Observation:
         r.raise_for_status()
         link_result = r.json()
 
-        result_list = list(link_result['_embedded'].values())[0]
-
-        if not allow_multiple:
-            entity_type = entity['content']['schema_type']
-            uuid = entity['uuid']['uuid']
-
-            if len(result_list) > 1:
-                raise IndexError(f'{entity_type} "{uuid}" should only return one entity from link "{link}".')
-
-            return result_list[0]
-
-        return result_list
+        return list(link_result['_embedded'].values())[0]
 
     @staticmethod
-    def __get_tree_to_donor(cell_suspension: dict):
-        tree = {}
-        cur_node = cell_suspension
+    def __get_type_of_entity(entity: dict) -> str:
+        return entity['content']['describedBy'].split('/')[-1]
+
+    def __build_biomaterial_chain(self) -> None:
+        logging.info(f'Building chain of biomaterials and protocols for cell suspension {self.cell_suspension_uuid}.')
+        # Build linked list of biomaterial -> protocol (?) -> biomaterial from lib prep protocol to donor organism
+        cell_suspension = self.__get_cell_suspension()
+        lib_prep = IngestObservation.__get_lib_prep_for_cell_suspension(cell_suspension)
+        lib_prep['child'] = cell_suspension
+
+        self.chain = lib_prep
+
+        cur_leaf = self.chain['child']  # cell_suspension
         while 1:
-            derived_by = Observation.__get_entity_from_link(cur_node, 'derivedByProcesses')
-            # ASSUMPTION: Just take first, could have multiple inputs
+            derived_by = IngestObservation.__get_entities_from_link(cur_leaf, 'derivedByProcesses')
+            if len(derived_by) > 1:
+                # ASSUMPTION: a biomaterial can only be derived by one process
+                logging.warning(
+                    f'Biomaterial {cur_leaf["uuid"]["uuid"]} is derived by multiple processes. Only using the first.'
+                )
+            derived_by = derived_by[0]
+
+            protocols_to_derive = IngestObservation.__get_entities_from_link(derived_by, 'protocols')
+            biomaterials_to_derive = IngestObservation.__get_entities_from_link(derived_by, 'inputBiomaterials')
+
+            if len(protocols_to_derive) > 0:
+                # Protocols may or may not exist for deriving a given biomaterial
+                if len(protocols_to_derive) > 1:
+                    # ASSUMPTION: a biomaterial can only be derived by one protocol
+                    logging.warning(
+                        f'Process {derived_by["uuid"]["uuid"]} has multiple protocols. Only using the first.'
+                    )
+
+                cur_leaf['child'] = protocols_to_derive[0]
+                cur_leaf = cur_leaf['child']
+                logging.info(
+                    f'Added {IngestObservation.__get_type_of_entity(cur_leaf)} {cur_leaf["uuid"]["uuid"]} to chain.'
+                )
+
             # (e.g. cell suspension may have multiple organoids) but they should share properties we care about
-            biomaterial = Observation.__get_entity_from_link(derived_by, 'inputBiomaterials', True)[0]
+            if len(biomaterials_to_derive) > 1:
+                # ASSUMPTION: A biomaterial can only be derived by one biomaterial
+                logging.warning(
+                    f'Process {derived_by["uuid"]["uuid"]} has multiple biomaterials. Only using the first.'
+                )
 
-            biomaterial_type = biomaterial['content']['describedBy'].split('/')[-1]
-            tree[biomaterial_type] = biomaterial
+            cur_leaf['child'] = biomaterials_to_derive[0]
+            cur_leaf = cur_leaf['child']
+            logging.info(
+                f'Added {IngestObservation.__get_type_of_entity(cur_leaf)} {cur_leaf["uuid"]["uuid"]} to chain.'
+            )
 
-            print(biomaterial_type)
-
-            if biomaterial_type == 'donor_organism':
+            if IngestObservation.__get_type_of_entity(cur_leaf) == 'donor_organism':
                 break
 
-            cur_node = biomaterial
+    def __get_tissue_ontology_term(self) -> Optional[str]:
+        to_try = ['organoid', 'cell_line', 'specimen_from_organism']
+        try:
+            for biomaterial_type in to_try:
+                biomaterial = self.__get_entity_with_type(biomaterial_type)
+                if biomaterial and biomaterial_type == to_try[0]:
+                    return biomaterial['content']['model_organ_part']['text']
+                if biomaterial and biomaterial_type == to_try[1]:
+                    return biomaterial['content']['tissue']['text']
+                if biomaterial and biomaterial_type == to_try[2]:
+                    return biomaterial['content']['organ_parts']['text']
+                return None
+        except KeyError:
+            return None
 
-        return tree
-
-
-    @staticmethod
-    def __compute_tissue_ontology_term(cell_suspension_tree):
-        keys = cell_suspension_tree.keys()
-        if 'organoid' in keys:
-            return  cell_suspension_tree['organoid']['content']['model_organ_part']['text']
-        if 'cell_line' in keys:
-            return cell_suspension_tree['cell_line']['content']['tissue']['text']
-        if 'specimen_from_organism' in keys:
-            return cell_suspension_tree['specimen_from_organism']['content']['organ_parts']['text']
-
-
-    @staticmethod
-    def from_hca_cell_suspension(biomaterial_uuid: str) -> 'Observation':
-        ingest_base = os.environ.get('INGEST_API')
-        print(ingest_base)
-        ingest_api = IngestApi(ingest_base)
-
-        # TODO refector to make a full tree structure with childs etc. Maybe use linked list
-        cell_suspension = ingest_api.get_entity_by_uuid('biomaterials', biomaterial_uuid)
-
-        # get library preparation protocol
-        input_to = Observation.__get_entity_from_link(cell_suspension, 'inputToProcesses', True)
-        lib_prep = None
-        for process in input_to:
-            output_protocols = Observation.__get_entity_from_link(process, 'protocols', True)
-            lib_preps = [x for x in output_protocols if 'library_preparation_protocol' in x['content']['describedBy']]
-
-            if len(lib_preps) > 1:
-                raise IndexError("Should only have one library preparation protocol per process.")
-
-            if not lib_prep:
-                lib_prep = lib_preps[0]
-            elif lib_preps[0]['uuid']['uuid'] != lib_prep['uuid']['uuid']:
-                raise TypeError("Cell suspension should only be associated to one library preparation protocol.")
-
-        input_tree = Observation.__get_tree_to_donor(cell_suspension)
-
-        # There must always be a donor and a specimen but cell line and organoid are optional parents
-        specimen_from_organism = input_tree['specimen_from_organism']
-        donor_organism = input_tree['donor_organism']
-
-        data = {
-            'assay_ontology_term_id': lib_prep['content']['library_construction_method']['text'],
-            'development_stage_ontology_term_id:human': donor_organism['content']['development_stage']['text'],
-            # ASSUMPTION: Use first disease, assuming all diseases are equal
-            'disease_ontology_term_id': specimen_from_organism['content']['diseases'][0]['text'],
-            'ethnicity_ontology_term_id:human': donor_organism['content']['human_specific']['ethnicity'][0]['text'],
-            'is_primary_data': True,
-            'organism_ontology_term_id': donor_organism['content']['genus_species'][0]['text'],
-            'sex_ontology_term_id': donor_organism['content']['sex'],
-            'tissue_ontology_term_id': Observation.__compute_tissue_ontology_term(specimen_from_organism)
-        }
-
-        return Observation(data)
-
-    def to_data_frame(self) -> DataFrame:
-        return pd.DataFrame(self.data, index=[0])
+    def __get_entity_with_type(self, entity_type: str) -> Optional[dict]:
+        cur_leaf = self.chain
+        while 1:
+            if IngestObservation.__get_type_of_entity(cur_leaf) == entity_type:
+                return cur_leaf
+            if 'child' not in cur_leaf:
+                return None
+            cur_leaf = cur_leaf['child']
