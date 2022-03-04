0a864eab32dc7c7f86143d45c12b625f22a015a0
Jacob Windsor <me@jcbwndsr.com>
2022-03-04 15:04:06 +0100

    dcp-687 rename goto and add set

--- hca_cellxgene/helpers/flat_chain.py
+++ hca_cellxgene/helpers/flat_chain.py
@@ -21,7 +21,7 @@ class FlatChain:
         self.__chain[key] = value
         self.__chain[self.__last_key]['child_key'] = key
 
-        self.set(key)
+        self.goto(key)
         self.__last_key = key
 
         return self
@@ -29,16 +29,24 @@ class FlatChain:
     def get_link(self, key: str) -> Optional[dict]:
         return self.__chain.get(key)
 
-    def set(self, key: str):
+    def goto(self, key: str) -> 'FlatChain':
         if key not in self.__chain:
             raise KeyError("Key not in chain")
 
         self.current = key, self.__chain[key]
+        return self
+
+    def set(self, key: str, value: dict) -> 'FlatChain':
+        if key not in self.__chain:
+            raise KeyError("Key not in chain")
+        
+        self.__chain[key] = value
+        return self
 
     def next(self) -> 'FlatChain':
         try:
             next_key = self.current[1]['child_key']
-            self.set(next_key)
+            self.goto(next_key)
         except KeyError:
             raise IndexError
         return self
@@ -46,7 +54,7 @@ class FlatChain:
     def prev(self) -> 'FlatChain':
         try:
             prev_key = self.current[1]['parent_key']
-            self.set(prev_key)
+            self.goto(prev_key)
         except KeyError:
             raise IndexError
         return self
