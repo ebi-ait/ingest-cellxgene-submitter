fd3b362a2c1e15d13c2820f34ec9c42c17fb940f
Jacob Windsor <me@jcbwndsr.com>
2022-03-14 23:38:21 +0100

    dcp-687 speed up by >6x by using threading

--- hca_cellxgene/H5AD.py
+++ hca_cellxgene/H5AD.py
@@ -1,5 +1,7 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
 from functools import partial
+from multiprocessing.pool import ThreadPool
 from pathlib import Path
 
 import pandas as pd
@@ -7,11 +9,12 @@ import anndata as ad
 import scipy.io as sio
 from dotenv import load_dotenv
 from pandas import SparseDtype, DataFrame
-from multiprocessing import Pool, Manager
+from multiprocessing import Manager
 
 from hca_cellxgene.observation import IngestObservation
 
 load_dotenv()
+
 
 def __load_barcodes(barcode_file_path) -> []:
     return pd.read_csv(filepath_or_buffer=barcode_file_path, header=None)[0].array
@@ -25,23 +28,29 @@ def __load_matrix(matrix_file_path) -> SparseDtype:
         return matrix
 
 
-def __build_obs_row(cache: dict, row: (str, str)) -> DataFrame:
-    index, cell_suspension_uuid = row
-    if cell_suspension_uuid not in cache:
-        cache[cell_suspension_uuid] = IngestObservation(cell_suspension_uuid, 'test')
-    return cache[cell_suspension_uuid].to_data_frame()
+def __build_obs_row(cell_suspension_uuid) -> (str, DataFrame):
+    return cell_suspension_uuid, IngestObservation(cell_suspension_uuid, 'test').to_data_frame()
 
 
 def __build_obs(input_csv_path) -> DataFrame:
-    rows = ((r[1]['index'], r[1]['uuid']) for r in pd.read_csv(input_csv_path).iterrows())
+    rows = [r[1]['uuid'] for r in pd.read_csv(input_csv_path).iterrows()]
 
-    with Manager() as m:
-        observation_cache = m.dict()
+    # Build the observation layer only for unique uuids, not for each row as each uuid may be duplicated multiple times
+    # Saves network requests
+    unique_uuids = set(rows)
+    with ThreadPoolExecutor() as executor:
+        unique_obs_rows = executor.map(__build_obs_row, unique_uuids)
 
-        with Pool() as p:
-            obs_layer_rows = p.map(partial(__build_obs_row, observation_cache), rows)
-            obs_layer = pd.concat(obs_layer_rows)
+    # Create a hashmap for convenience in later lookup
+    unique_obs_hashmap = {x[0]: x[1] for x in unique_obs_rows}
 
+    # Go through each row in the original file and get the created observations
+    total_obs_rows = []
+    for uuid in rows:
+        total_obs_rows.append(unique_obs_hashmap[uuid])
+
+    # Build the data frame from this result
+    obs_layer = pd.concat(total_obs_rows)
     return obs_layer
 
 
