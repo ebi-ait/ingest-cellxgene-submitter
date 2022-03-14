1a6fe050fdba08b6853cb235fb9b7360bd434c60
Jacob Windsor <me@jcbwndsr.com>
2022-03-14 17:12:22 +0100

    dcp-687 paralelize

--- hca_cellxgene/H5AD.py
+++ hca_cellxgene/H5AD.py
@@ -5,11 +5,14 @@ import pandas as pd
 import anndata as ad
 import scipy.io as sio
 from dotenv import load_dotenv
-from pandas import SparseDtype
+from pandas import SparseDtype, DataFrame
+from multiprocessing import Pool
 
 from hca_cellxgene.observation import IngestObservation
 
 load_dotenv()
+
+OBSERVATION_CACHE = {}
 
 
 def __load_barcodes(barcode_file_path) -> []:
@@ -24,18 +27,20 @@ def __load_matrix(matrix_file_path) -> SparseDtype:
         return matrix
 
 
+def __build_obs_row(row: (str, str)) -> DataFrame:
+    index, cell_suspension_uuid = row
+    if cell_suspension_uuid not in OBSERVATION_CACHE:
+        OBSERVATION_CACHE[cell_suspension_uuid] = IngestObservation(cell_suspension_uuid, 'test')
+    return OBSERVATION_CACHE[cell_suspension_uuid].to_data_frame()
+
+
 def generate(input_csv_path, matrix_file_path):
-    rows = ((r['index'], r['uuid'], r['type']) for r in pd.read_csv(input_csv_path).iterrows())
+    rows = ((r[1]['index'], r[1]['uuid']) for r in pd.read_csv(input_csv_path).iterrows())
 
-    observations = {} # Some rows may have duplicated uuids so cache results of IngestObservation
-    obs_layer_rows = []
-    for index, cell_suspension_uuid, cell_type in rows:
-        if cell_suspension_uuid not in observations:
-            observations[cell_suspension_uuid] = IngestObservation(cell_suspension_uuid, cell_type)
-        obs_layer_rows.append(observations[cell_suspension_uuid].to_data_frame())
-
-    obs_layer = pd.concat(obs_layer_rows)
-    matrix = __load_matrix(matrix_file_path)
-    h5ad = ad.AnnData(matrix, obs_layer)
-    h5ad.write(Path(os.environ['OUTPUT_PATH'], 'output.h5ad'))
+    with Pool() as p:
+        obs_layer_rows = p.map(__build_obs_row, rows)
+        obs_layer = pd.concat(obs_layer_rows)
+        matrix = __load_matrix(matrix_file_path)
+        h5ad = ad.AnnData(matrix, obs_layer)
+        h5ad.write(Path(os.environ['OUTPUT_PATH'], 'output.h5ad'))
 
