e8bce4fb5c0fbf1c840a27a4b8b0fecab5c817fc
Jacob Windsor <me@jcbwndsr.com>
2022-03-15 12:16:32 +0100

    dcp-687 Refactor to take rows directly as input not CSV

--- hca_cellxgene/H5AD.py
+++ hca_cellxgene/H5AD.py
@@ -1,15 +1,12 @@
 import os
 from concurrent.futures import ThreadPoolExecutor
-from functools import partial
-from multiprocessing.pool import ThreadPool
 from pathlib import Path
 
-import pandas as pd
 import anndata as ad
+import pandas as pd
 import scipy.io as sio
 from dotenv import load_dotenv
 from pandas import SparseDtype, DataFrame
-from multiprocessing import Manager
 
 from hca_cellxgene.observation import IngestObservation
 
@@ -32,12 +29,10 @@ def __build_obs_row(cell_suspension_uuid) -> (str, DataFrame):
     return cell_suspension_uuid, IngestObservation(cell_suspension_uuid, 'test').to_data_frame()
 
 
-def __build_obs(input_csv_path) -> DataFrame:
-    rows = [r[1]['uuid'] for r in pd.read_csv(input_csv_path).iterrows()]
-
+def __build_obs(uuids: [str]) -> DataFrame:
     # Build the observation layer only for unique uuids, not for each row as each uuid may be duplicated multiple times
     # Saves network requests
-    unique_uuids = set(rows)
+    unique_uuids = set(uuids)
     with ThreadPoolExecutor() as executor:
         unique_obs_rows = executor.map(__build_obs_row, unique_uuids)
 
@@ -46,7 +41,7 @@ def __build_obs(input_csv_path) -> DataFrame:
 
     # Go through each row in the original file and get the created observations
     total_obs_rows = []
-    for uuid in rows:
+    for uuid in uuids:
         total_obs_rows.append(unique_obs_hashmap[uuid])
 
     # Build the data frame from this result
@@ -54,12 +49,18 @@ def __build_obs(input_csv_path) -> DataFrame:
     return obs_layer
 
 
-def generate_obs(input_csv_path):
-    __build_obs(input_csv_path).to_csv(Path(os.environ['OUTPUT_PATH'], 'obs.csv'))
+def __read_input_csv(input_csv_path) -> [str]:
+    return [r[1]['uuid'] for r in pd.read_csv(input_csv_path).iterrows()]
 
 
-def generate(input_csv_path, matrix_file_path):
-    obs_layer = __build_obs(input_csv_path)
+def generate_obs(input_csv_path: os.PathLike):
+    rows = __read_input_csv(input_csv_path)
+    __build_obs(rows).to_csv(Path(os.environ['OUTPUT_PATH'], 'obs.csv'))
+
+
+def generate(input_csv_path: os.PathLike, matrix_file_path: os.PathLike):
+    rows = __read_input_csv(input_csv_path)
+    obs_layer = __build_obs(rows)
     matrix = __load_matrix(matrix_file_path)
     h5ad = ad.AnnData(matrix, obs_layer)
     h5ad.write(Path(os.environ['OUTPUT_PATH'], 'output.h5ad'))
